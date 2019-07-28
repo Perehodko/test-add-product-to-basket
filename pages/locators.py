5b56dddd702e346abaa5fa698cd82e62c66e29b9
Perehodko <n.perehodko@yandex.ru>
2019-07-28 19:55:36 +0300

    add locators

---
+++ pages/locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+class BasePageLocators(object):
+    LOGIN_LINK = (By.XPATH, "//button [text()='Add to basket']")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link")
+    NAME_OF_BOOK = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > h1")
+    NAME_OF_BOOK_IN_BASKET = (By.CSS_SELECTOR,"#messages > div:nth-child(1) > div > strong")
+    PRICE_BOOK = (By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > p.price_color")
+    PRICE_BOOK_IN_BASKET = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages > div.alert.alert-safe.alert-noicon.alert-info.fade.in > div > p:nth-child(1) > strong")
